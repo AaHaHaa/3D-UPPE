1fcec80ab3c3029bf8703101dfc98c5de4c2fc8d
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2025-03-07 10:05:43 -0500

    Update some codes

--- Examples/Multipass cell/Mode-coupling-suppresion bulk MPC (silica-plate MPC)/Multipass_cell_r.m
+++ Examples/Multipass cell/Mode-coupling-suppresion bulk MPC (silica-plate MPC)/Multipass_cell_r.m
@@ -1,30 +1,30 @@
-% This code simulates a 12-plate PKLM compressor of a Gaussian pulse.
-% The medium is N-SF11.
-% A PLKM-based compressor is used to establish a discrete waveguide for
-% nonlinear pulse compressor, which, here, compresses a 310-fs pulse to <80
-% fs.
+% This code simulates a multipass cell and aims to duplicate
+% 
+% 1. First experimental demonstration of MPC
+%    
+%   Schulte et al., "Nonlinear pulse compression in a multi-pass cell," Opt. Lett. 41(19), 4511-4514 (2016)
 %
-% Please check our paper for details:
-%   Wang et al., "Efficient temporal compression of 10-μJ pulses in
-%   periodic layered Kerr media," Opt. Lett. 49(20), 5787-5790 (2024)
+% 2. Its latter simulation (Fig. 4 of the reference paper below):
+%
+%   Hanna et al., "Nonlinear temporal compression in multipass cells: theory," J. Opt. Soc. Am. B 34(7), 1340-1347 (2017)
 %
 % This script employs the radially-symmetric scheme of the UPPE code, 
 % rather than a full x-y dimension.
 
 clearvars; close all;
 
-addpath('../../UPPE3D algorithm/','../../user_helpers/'); % add package paths
+addpath('../../../UPPE3D algorithm/','../../../user_helpers/'); % add package paths
 
 %% Setup temporal/spectral parameters
-Nt = 2^8; % the number of time points
+Nt = 2^11; % the number of time points
 time_window = 2; % ps
 dt = time_window/Nt; % ps
 
 %% Setup spatial parameters
 % Information for the Hankel transform
-Nr = 2^10; % the number of radial sampling points
-r_max = 0.9e-3; % the maximum radius; half of the spatial window
-kr_max = 1.5e5; % the maximum kr vector
+Nr = 2^11; % the number of radial sampling points
+r_max = 6e-3; % the maximum radius; half of the spatial window
+kr_max = 5e4; % the maximum kr vector
 FHATHA_energy_restoration = false; % don't enable constant operations of FHATHA's energy restoration function (see details of FHATHA in readme)
 
 [r,kr,...
@@ -43,9 +43,8 @@ sim.FHATHA = struct('r',r,'kr',kr,...
 %% Setup simulation parameters
 sim.lambda0 = 1030e-9; % m; the center wavelength
 sim.gpuDevice.Index = 1; % choose which GPU to use if you have multiple GPUs: 1,2,3...
-sim.include_Raman = false; % N-SF11's Raman isn't considered in this case
 
-fiber.material = 'N-SF11'; % for finding the refractive index in UPPE3D_propagate()
+fiber.material = 'silica'; % for finding the Raman model in UPPE3D_propagate()
 
 % Please check this function for details.
 [fiber,sim] = load_default_UPPE3D_propagate(fiber,sim); % load default parameters
@@ -63,101 +62,129 @@ lambda = c./(f*1e12)*1e9; % nm
 Sellmeier_terms = @(lambda,a,b) a.*lambda.^2./(lambda.^2 - b.^2);
 n_from_Sellmeier = @(lambda) sqrt(1+sum(Sellmeier_terms(lambda,a,b),2));
 
-% Plate material properties
-n_plate = n_from_Sellmeier(lambda/1e3); % refractive index
-n2_plate = (10)*1e-20; % m^2/W; nonlinear refractive index
+% silica material properties
+n_silica = 1;%n_from_Sellmeier(lambda/1e3); % refractive index
+n2_silica = 2.3e-20; % m^2/W; nonlinear refractive index
 
 % Air properties
 n_air = 1; % air's n=1
 n2_air = 0; % no nonlinearity
 
-%% Setup PLKM parameters
-num_plates = 12;
-Plate_thickness = 0.5e-3; % m
-Plate_spacing = 10.3e-3; % m
-D = 12e-3; % m; distance between the focal point and the first plate
+%% Setup MPC parameters
+num_roundtrip = 10;
+MPC_length = 0.795; % m
+focal_R = 0.432; % m; radius of curvature of the mirror
+focal_length = focal_R/2; % m
+silica_thickness = 27.1e-2; % m
 
-plate_z = D;
-for i = 2:num_plates*2+1
-    if mod(i,2) == 0
-        zi = Plate_thickness;
-    else
-        zi = Plate_spacing;
+plate_z = MPC_length/2;
+for i = 1:num_roundtrip*6
+    switch mod(i,6)
+        case {0,1,3,4}
+            zi = (MPC_length - silica_thickness)/2;
+        case {2,5}
+            zi = silica_thickness;
     end
     plate_z = [plate_z;plate_z(end)+zi];
 end
 
 %% Initial condition
-MFD0 = 120e-6; % m; mode-field diameter
-tfwhm = 0.31; % ps
-energy = ((20)*0.85)*1e3; % nJ
+MFD0 = 2*((focal_R^2-(MPC_length-focal_R)^2)*sim.lambda0^2/4/pi^2)^(1/4); % m; mode-field diameter; 450um, calculated from Eq.(5) in the reference paper
+tfwhm = 0.17; % ps
+energy = 1.32e3; % nJ
 initial_condition = build_3Dgaussian_r(MFD0, tfwhm, time_window, energy, Nt, r);
 
 %%
 % =========================================================================
-% Start the simulation of PLKM
+% Start the simulation of MPC
 % =========================================================================
-num_save = 10;
-z_all = zeros(num_save,2*num_plates+1);
-MFD_all = zeros(num_save,2*num_plates+1);
+num_save = 5;
+z_all = zeros(num_save,6*num_roundtrip+2);
+MFD_all = zeros(num_save,6*num_roundtrip+2);
 
-Frame(num_save,2*num_plates+1) = struct('cdata',[],'colormap',[]);
+Frame(num_save,6*num_roundtrip+1) = struct('cdata',[],'colormap',[]);
 
-%% air0; before the beam hits the first plate
-fiber.L0 = D; % m
+%% Before hitting the first mirror
+fiber.L0 = MPC_length/2; % m
 fiber.n =  n_air;
 fiber.n2 = n2_air;
 
 sim.save_period = fiber.L0/num_save;
+
+% Show initial k space
+A0_H = 2*pi*FHATHA(squeeze(initial_condition.field(floor(Nt/2)+1,:,end)),...
+                   r_max,...
+                   r,kr,...
+                   dr,dkr,...
+                   l0,exp_prefactor,n2_prefactor,...
+                   ifftQ);
+fig_k = figure;
+plot(kr/1e3,abs(A0_H).^2,'linewidth',2,'Color','r');
+xlabel('k_r (2\pi/mm)');
+set(gca,'fontsize',20);
+title('Initial k space');
+% Plot the 2D field with pcolor
+fig_k2 = radialPcolor(kr/1e3,abs(A0_H).^2);
+xlabel('k_x (2\pi/mm)');
+ylabel('k_y (2\pi/mm)');
+set(gca,'fontsize',20);
+daspect([1 1 1]); % make aspect ratio = 1
+title('Initial k space');
 
 % Simulate the propagation
 prop_output = UPPE3D_propagate(fiber,initial_condition,sim);
 
 % Calculate MFD during propagation
 z_all(:,1) = prop_output.z(2:end); % the first one is the input z=0, so ignore it
-remove_noise_model = 2; % see calcMFD_r for details
-MFD = calcMFD_r(squeeze(prop_output.field(Nt/2,:,:)),r,remove_noise_model)'*1e3; % mm
+remove_noise_model = 2; % see calcMFD for details
+MFD = calcMFD_r(squeeze(sqrt(sum(abs(prop_output.field).^2,1))),r,remove_noise_model)'*1e3; % mm
 MFD_all(:,1) = MFD(2:end); % the first one is the input, so ignore it
 fprintf('air %u\n',0);
 num_to_plot = num_save;
 fig = plotter_r([],...
                 prop_output.field,...
                 z_all(1:num_to_plot),MFD_all(1:num_to_plot),...
+                num_save,...
                 r,lambda);
 
 % Animation
 Frame(:,1) = animator_r(Frame(:,1),...
                         prop_output.field,...
                         z_all(1:num_to_plot),MFD_all(1:num_to_plot),0,...
+                        num_save,...
                         Nt,dt,r,lambda,...
                         plate_z);
 
-%% PLKM: plate, air, plate, air,......
-for i = 1+(1:num_plates*2)
-    initial_condition.field = prop_output.field(:,:,end);
+%% MPC
+for i = 1+(1:num_roundtrip*6)
+    initial_condition.field = prop_output.field(:,:,:,end);
+
+    if ismember(mod(i-1,6),[1,4])
+        Ef_reflected = add_thin_lens_phase_r(ifft(initial_condition.field,[],1),initial_condition.r,ifftshift(lambda,1)/1e9,focal_length);
+        initial_condition.field = fft(Ef_reflected,[],1);
+    end
     
-    if mod(i,2) == 0 % plate
-        fiber.L0 = Plate_thickness; % m
-        fiber.n =  n_plate;
-        fiber.n2 = n2_plate;
-    else % mod(i,2) == 1; air
-        fiber.L0 = Plate_spacing;
-        fiber.n = n_air; % air
-        fiber.n2 = n2_air; % no nonlinearity
+    switch mod(i-1,6)
+        case {0,1,3,4}
+            fiber.L0 = (MPC_length - silica_thickness)/2;
+            fiber.n = n_air; % air
+            fiber.n2 = n2_air; % no nonlinearity
+        case {2,5}
+            fiber.L0 = silica_thickness;
+            fiber.n =  n_silica;
+            fiber.n2 = n2_silica;
     end
     
     sim.save_period = fiber.L0/num_save;
 
     % Show initial k space
-    A0_H = 2*pi*FHATHA(squeeze(initial_condition.field(floor(Nt/2)+1,:)),...
+    close(fig_k,fig_k2);
+    A0_H = 2*pi*FHATHA(squeeze(initial_condition.field(floor(Nt/2)+1,:,end)),...
                        r_max,...
                        r,kr,...
                        dr,dkr,...
                        l0,exp_prefactor,n2_prefactor,...
                        ifftQ);
-    if exist('fig_k','var')
-        close(fig_k); close(fig_k2);
-    end
     fig_k = figure;
     plot(kr/1e3,abs(A0_H).^2,'linewidth',2,'Color','r');
     xlabel('k_r (2\pi/mm)');
@@ -176,31 +203,34 @@ for i = 1+(1:num_plates*2)
     
     % Calculate MFD during propagation
     z_all(:,i) = prop_output.z(2:end) + z_all(end,i-1); % the first one is the input z=0, so ignore it
-    MFD = calcMFD_r(squeeze(prop_output.field(Nt/2,:,:)),r,remove_noise_model)'*1e3; % mm
+    MFD = calcMFD_r(squeeze(sqrt(sum(abs(prop_output.field).^2,1))),r,remove_noise_model)'*1e3; % mm
     MFD_all(:,i) = MFD(2:end); % the first one is the input, so ignore it
-    if mod(i,2) == 0 % plate
-        fprintf('plate %u\n',i/2);
-    else
-        fprintf('air %u\n',floor(i/2));
+    switch mod(i-1,6)
+        case {0,1,3,4}
+            fprintf('air %u\n',floor((i-1)/6));
+        case {2,5}
+            fprintf('silica %u\n',floor((i-1)/6));
     end
     num_to_plot = i*num_save;
     fig = plotter_r(fig,... ...
                     prop_output.field,...
                     z_all(1:num_to_plot),MFD_all(1:num_to_plot),...
+                    num_save,...
                     r,lambda);
 
     % Animation
     Frame(:,i) = animator_r(Frame(:,i),...
                             prop_output.field,...
                             z_all(1:num_to_plot),MFD_all(1:num_to_plot),(i-1)*num_save,...
+                            num_save,...
                             Nt,dt,r,lambda,...
                             plate_z);
 end
 
 % Movie
-implay(Frame(:),20);
-exportVideo = VideoWriter('PLKM_r');
-exportVideo.FrameRate = 20;
+implay(Frame(:),30);
+exportVideo = VideoWriter('MPC_r');
+exportVideo.FrameRate = 30;
 open(exportVideo);
 writeVideo(exportVideo, Frame(:));
 close(exportVideo);
---
+++ Examples/Multipass cell/Mode-coupling-suppresion bulk MPC (silica-plate MPC)/animator_r.m
@@ -0,0 +1,79 @@
+function Frame = animator_r(Frame,...
+                            A,...
+                            z,MFD,start_idx,...
+                            num_save,...
+                            Nt,dt,r,lambda,...
+                            plate_z)
+
+% Make them column vectors
+z = z(:);
+MFD = MFD(:);
+
+c = 299792.458; % nm/ps
+factor_correct_unit = (Nt*dt)^2/1e3; % to make the spectrum of the correct unit "nJ/THz"
+                                     % "/1e3" is to make pJ into nJ
+factor = c./lambda.^2; % change the spectrum from frequency domain into wavelength domain
+
+plot_wavelength_lim = [850,1200];
+plot_MFD_lim = [0,1500];
+for j = 1:size(A,3)-1
+    if exist('fig','var')
+        figure(fig);
+    else
+        fig = figure;
+    end
+
+    spectrum = abs(fftshift(ifft(A(:,:,j+1),[],1),1)).^2*factor_correct_unit.*factor; % nJ/nm/m^2
+
+    subplot(2,2,1);
+    pcolor(lambda,r*1e6,spectrum');
+    shading interp; colormap(jet);
+    xlabel('Wavelength (nm)');
+    ylabel('r (\mum)');
+    xlim(plot_wavelength_lim);
+    ylim([0,plot_MFD_lim(2)]);
+
+    subplot(2,2,2);
+    %plot(plate_z(1)*1e2*[1;1],plot_MFD_lim,'Color','m','linewidth',0.3);
+    plot(plate_z(2)*1e2*[1;1],plot_MFD_lim,'Color','b','linewidth',1.5);
+    hold on;
+    for i = 3:length(plate_z)
+        switch mod(i-2,6)
+            case {1,2,4,5}
+                %plot(plate_z(i)*1e2*[1;1],plot_MFD_lim,'Color','m','linewidth',0.5);
+            case {0,3}
+                plot(plate_z(i)*1e2*[1;1],plot_MFD_lim,'Color','b','linewidth',1.5);
+        end
+        hold on;
+    end
+    plot(z(1:start_idx+j)*1e2,MFD(1:start_idx+j)*1e3,'Color','k','linewidth',2);
+    for i = 1:length(MFD)/num_save
+        if ismember(mod(i-1,6),[2,5])
+            plot(z((i-1)*num_save:i*num_save)*1e2,MFD((i-1)*num_save:i*num_save)*1e3,'Color','r','linewidth',2);
+        end
+    end
+    hold off;
+    xlabel('z (cm)');
+    ylabel('MFD (\mum)');
+    xlim([0,plate_z(end)*1e2]); % cm
+    ylim(plot_MFD_lim);
+
+    subplot(2,2,[3,4]);
+    avg_spectrum = 2*pi*trapz(r,spectrum.*r,2); % nJ/nm
+    plot(lambda,avg_spectrum,'Color','b','linewidth',2);
+    hold on;
+    plot(lambda,spectrum(:,1)/max(spectrum(:,1))*max(avg_spectrum),'Color','r','linewidth',2);
+    hold off;
+    xlabel('Wavelength (nm)');
+    ylabel('PSD (nJ/nm)');
+    xlim(plot_wavelength_lim);
+    ylim([0,max(avg_spectrum)]);
+    legend('Avg spectrum','Center spectrum (norm.)');
+
+    set(fig,'Color',[1,1,1]);
+
+    Frame(j) = getframe(fig);
+end
+close(fig);
+
+end
